08e9b2085cf163de37c7c1ef6ddfc9e81220f8f8
Karel <charles.mccoy@seznam.cz>
2019-12-07 15:05:59 +0100

    HelloWorld excersises completed

---
+++ week-03/05-HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // HelloMe
+            Console.WriteLine("Hello, Karel!\n");
+
+            // HumptyDumpty
+            Console.WriteLine("Humpty Dumpty sat on a wall,");
+            Console.WriteLine("Humpty Dumpty had a great fall.");
+            Console.WriteLine("All the king's horses and all the king's men");
+            Console.WriteLine("Couldn't put Humpty together again.\n");
+
+            // HelloOthers
+            Console.WriteLine("Hello, Igor!");
+            Console.WriteLine("Hello, Petra!");
+            Console.WriteLine("Hello, Dan!");
+        }
+    }
+}
