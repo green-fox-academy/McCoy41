049764857671783eb8d0f069f20eb09875d2a216
Karel <charles.mccoy@seznam.cz>
2019-12-14 15:13:33 +0100

    BMI - fancier solution for FatnessLevel

--- week-03/09-Bmi/Program.cs
+++ week-03/09-Bmi/Program.cs
@@ -12,11 +12,15 @@ namespace _09_Bmi
             string level;
 
             Console.WriteLine("Welcome to BMI Calculator!");
+
+            // ask for data
             Console.Write("Please input your height (in meters): ");
             height = Convert.ToDouble(Console.ReadLine());
             Console.Write("Please input your weight (in kilograms): ");
             weight = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("\nInputted Values: " + height + "m, " + weight + "kg.");
+
+            //calculate bmi
             bmi = weight / (height * height);
             level = FatnessLvl(bmi);
             Console.WriteLine("\nYour BMI Index is: {0:0.0}", bmi);
@@ -24,10 +28,11 @@ namespace _09_Bmi
             FatnessList();
         }
 
-        static string FatnessLvl(double bmi)
+        static string FatnessLvl(double bmi) // returns fatness level based on BMI
         {
             string[] level = { "SKELLY", "SKINNY", "PRETTY NORMAL", "BIG", "HEALTHY", "HUSKY", "FLUFFY", "DAAAMN!!!" };
 
+            /* easy solution
             if (bmi < 18) { return level[0]; }
             else if (bmi < 23) { return level[1]; }
             else if (bmi < 25) { return level[2]; }
@@ -36,11 +41,30 @@ namespace _09_Bmi
             else if (bmi < 40) { return level[5]; }
             else if (bmi < 50) { return level[6]; }
             else { return level[7]; }
+            */
+
+            // fancy solution
+            int[] maxbmi = { 18, 23, 25, 30, 35, 40, 50 };
+            int i = 0;
+            bool next = true;
+            while (i < 7 && next == true)
+            {
+                if (bmi < maxbmi[i])
+                {
+                    next = false;
+                    return level[i];
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            return level[7];
         }
 
-        static void FatnessList()
+        static void FatnessList() // list of all levels
         {
-            string[] level = { "skelly", "skinny", "pretty normal", "big", "healthy", "husky", "fluffy", "DAYUM!" };
+            string[] level = { "skelly", "skinny", "pretty normal", "big", "healthy", "husky", "fluffy", "DAMN!" };
             string[] range = { "<18,0", "18,0 - 22,9", "23,0 - 24,9", "25,0 - 29,9", "30,0 - 34,9", "35,0 - 39,9", "40,0 - 49,9", ">50,0" };
 
             Console.WriteLine("\nAvailable levels of fatness are:");
