c1132f318dfa28ef3f81279cecf695a9c9090a55
Karel <charles.mccoy@seznam.cz>
2019-12-14 14:13:05 +0100

    Cuboid - fixed typos

--- week-04/10-Cuboid-Cube/Program.cs
+++ week-04/10-Cuboid-Cube/Program.cs
@@ -9,22 +9,19 @@ namespace _10_Cuboid_Cube
             double s;
             double v;
             double a;
-            
-            //v = a * a * a;
-            //s = 6 * a * a;
 
             Console.WriteLine("WELCOME TO THE CUBELATOR\n");
             Console.WriteLine("Types of input:\n" +
-                               "a = input lenght of the cube edge\n" +
-                               "s = input surface area of the cube\n" +
-                               "v = input volume of the area\n");
+                               " a = input lenght of the cube edge\n" +
+                               " s = input surface area of the cube\n" +
+                               " v = input volume of the area\n");
             Console.WriteLine("(Press <ESC> or <ENTER> to exit the program)\n");
             
             while (true)
             {
                 Console.Write("\nPlease select type of input: ");
                 var key = Console.ReadKey().Key;
-                if (key == ConsoleKey.A)
+                if (key == ConsoleKey.A) // EDGE
                 {
                     Console.Write("\nPlease insert lenght of the cube edge (in centimeters): ");
                     a = Convert.ToDouble(Console.ReadLine());
@@ -32,27 +29,27 @@ namespace _10_Cuboid_Cube
                     v = a * a * a;
                     Output(a, s, v);
                 }
-                else if (key == ConsoleKey.S)
+                else if (key == ConsoleKey.S) // SURFACE
                 {
-                    Console.Write("\nPlease insert lenght of the cube edge (in square centimeters): ");
+                    Console.Write("\nPlease insert surface area of the cube (in square centimeters): ");
                     s = Convert.ToDouble(Console.ReadLine());
                     a = Math.Sqrt(s / 6);
                     v = s * a / 6;
                     Output(a, s, v);
                 }
-                else if (key == ConsoleKey.V)
+                else if (key == ConsoleKey.V) // VOLUME
                 {
-                    Console.Write("\nPlease insert lenght of the cube edge (in cubic centimeters): ");
+                    Console.Write("\nPlease insert volume of the cube (in cubic centimeters): ");
                     v = Convert.ToDouble(Console.ReadLine());
                     a = Math.Pow(v, 1.0 / 3.0);
                     s = 6 * a * a;
                     Output(a, s, v);
                 }
-                else if (key == ConsoleKey.Escape || key == ConsoleKey.Enter)
+                else if (key == ConsoleKey.Escape || key == ConsoleKey.Enter) // EXIT
                 {
                     break;
                 }
-                else
+                else // UNKNOWN
                 {
                     Console.WriteLine("\nCalculation not recognized!");
                 }
