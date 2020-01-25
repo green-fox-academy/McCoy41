26d93952bcd1779816ce0dbcf0a3d3d1f68c8647
Karel <charles.mccoy@seznam.cz>
2020-01-25 14:54:52 +0100

    DrawingProject LinePlay completed

---
+++ week-07/15-DrawingProject/FoxDraw.cs
@@ -0,0 +1,116 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Shapes;
+using Avalonia.Media;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GreenFox
+{
+    public class FoxDraw
+    {
+        private Canvas Canvas { get; set; }
+        private SolidColorBrush LineColor { get; set; } = new SolidColorBrush(Colors.Black);
+        private SolidColorBrush ShapeColor { get; set; } = new SolidColorBrush(Colors.DarkGreen);
+
+        private int StrokeThickness { get; set; } = 1;
+
+        public FoxDraw(Canvas canvas)
+        {
+            Canvas = canvas;
+        }
+
+        public void SetBackgroundColor(Color color)
+        {
+            Canvas.Background = new SolidColorBrush(color);
+        }
+
+        public void SetStrokeThicknes(int thickness)
+        {
+            StrokeThickness = thickness;
+        }
+
+        public void SetStrokeColor(Color color)
+        {
+            LineColor = new SolidColorBrush(color);
+        }
+
+        public void SetFillColor(Color color)
+        {
+            ShapeColor = new SolidColorBrush(color);
+        }
+
+        public void DrawEllipse(double x, double y, double width, double height)
+        {
+            var ellipse = new Ellipse()
+            {
+                Stroke = LineColor,
+                StrokeThickness = StrokeThickness,
+                Fill = ShapeColor,
+                Width = width,
+                Height = height
+            };
+
+            Canvas.Children.Add(ellipse);
+            SetPosition(ellipse, x, y);
+        }
+
+        public void DrawLine(Point start, Point end)
+        {
+            var line = new Line()
+            {
+                Stroke = LineColor,
+                StrokeThickness = StrokeThickness,
+                StartPoint = start,
+                EndPoint = end
+            };
+
+            Canvas.Children.Add(line);
+        }
+
+        public void DrawLine(double x1, double y1, double x2, double y2)
+        {
+            DrawLine(new Point(x1, y1), new Point(x2, y2));
+        }
+
+        public void DrawPolygon(IEnumerable<Point> points)
+        {
+            var polygon = new Polygon()
+            {
+                Stroke = LineColor,
+                StrokeThickness = StrokeThickness,
+                Fill = ShapeColor,
+                Points = points.ToList()
+            };
+
+            Canvas.Children.Add(polygon);
+        }
+
+        public void DrawRectangle(double x, double y, double width, double height)
+        {
+            var rectangle = new Rectangle()
+            {
+                Stroke = LineColor,
+                StrokeThickness = StrokeThickness,
+                Fill = ShapeColor,
+                Width = width,
+                Height = height
+            };
+
+            Canvas.Children.Add(rectangle);
+            SetPosition(rectangle, x, y);
+        }
+
+        public void AddImage(Image image, double x, double y)
+        {
+            Canvas.Children.Add(image);
+            SetPosition(image, x, y);
+        }
+
+        public void SetPosition(AvaloniaObject shape, double x, double y)
+        {
+            Canvas.SetLeft(shape, x);
+            Canvas.SetTop(shape, y);
+        }
+    }
+}
---
+++ week-07/15-DrawingProject/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using GreenFox;
+
+namespace _15_DrawingProject
+{
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+            var canvas = this.Get<Canvas>("canvas");
+            var foxDraw = new FoxDraw(canvas);
+            Width = 600;
+            Height = 600;
+
+            // HERE GOES THE CODE
+
+            int detail = 30;
+            int size = 20;
+            foxDraw.SetStrokeThicknes(2);
+
+            for (int a = 0; a <= detail; a++)
+            {
+                foxDraw.SetStrokeColor(Colors.LimeGreen);
+                foxDraw.DrawLine(0, a * size, a * size, detail * size);
+                foxDraw.SetStrokeColor(Colors.Purple);
+                foxDraw.DrawLine(detail * size, (detail - a) * size, (detail - a) * size, 0);
+            }
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
