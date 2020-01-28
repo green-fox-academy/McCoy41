f4e309705e2a6813e8f5e34cfefa6cd1142d1795
Karel <charles.mccoy@seznam.cz>
2020-01-28 10:59:37 +0100

    TextEncoder: added string-encoding functions

--- week-08/16-TextEncoder/Program.cs
+++ week-08/16-TextEncoder/Program.cs
@@ -6,13 +6,13 @@ namespace _16_TextEncoder
 {
     class Program
     {
-        
+        enum EncodeType { StringMirror, StringOrder, StringCombine, CharMulti, CharShift, None }
         static void Main(string[] args)
         {
             List<string> txtFile = new List<string>();
             List<char> charList = new List<char>();
             charList = LoadFile("template", 0);
-            txtFile = LoadFile("template");
+            txtFile = Encode(LoadFile("template"),EncodeType.StringCombine);
 
             foreach (char character in charList)
             {
@@ -62,6 +62,65 @@ namespace _16_TextEncoder
             {
                 Console.WriteLine(e.Message);
             }
+            return output;
+        }
+        static List<string> Encode(List<string> input, EncodeType type)
+        {
+            List<string> output = new List<string>(input.Count);
+            switch (type)
+            {
+                case EncodeType.StringMirror:
+                    return StringMirror(input);
+                case EncodeType.StringOrder:
+                    return StringOrder(input);
+                case EncodeType.StringCombine:
+                    return StringOrder(StringMirror(input));
+                default:
+                    return input;
+            }
+        }
+        static List<char> Encode(List<char> input, EncodeType type)
+        {
+            switch (type)
+            {
+                case EncodeType.CharMulti:
+                    return input;
+                case EncodeType.CharShift:
+                    return input;
+                default:
+                    return input;
+            }
+        }
+        static List<string> StringMirror(List<string> input)
+        {
+            List<string> output = new List<string>(input.Count);
+            foreach (string line in input)
+            {
+                string encodedLine = "";
+                try
+                {
+                    for (int i = 1; i <= line.Length; i++)
+                    {
+                        encodedLine += line[^i];
+                    }
+                }
+                catch (NullReferenceException)
+                {
+                    output.Add("");
+                }
+                output.Add(encodedLine);
+            }
+            return output;
+        }
+        static List<string> StringOrder(List<string> input)
+        {
+            List<string> output = new List<string>(input.Count);
+            
+            for(int i = input.Count - 1; i >= 0; i--)
+            {
+                output.Add(input[i]);
+            }
+
             return output;
         }
     }
