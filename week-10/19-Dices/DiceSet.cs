55f2be87f2ee878f1a6c64caa6567d92d670b67e
Karel <charles.mccoy@seznam.cz>
2020-02-11 11:11:44 +0100

    Dices exercise completed

---
+++ week-10/19-Dices/DiceSet.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _19_Dices
+{
+    class DiceSet
+    {
+        private Random randomValue = new Random();
+        private int[] dice = new int[6];
+
+        public int[] Roll()
+        {
+            for (int i = 0; i < dice.Length; i++)
+            {
+                dice[i] = randomValue.Next(1, 7);
+            }
+
+            return dice;
+        }
+
+        public int[] GetCurrent()
+        {
+            return dice;
+        }
+
+        public int GetCurrent(int i)
+        {
+            return dice[i];
+        }
+
+        public void Reroll()
+        {
+            for (int i = 0; i < dice.Length; i++)
+            {
+                dice[i] = randomValue.Next(1, 7);
+            }
+        }
+
+        public void Reroll(int k)
+        {
+            dice[k] = randomValue.Next(1, 7);
+        }
+
+    }
+}
---
+++ week-10/19-Dices/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+namespace _19_Dices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DiceSet diceSet = new DiceSet();
+            diceSet.Roll();
+            for (int i = 0; i < diceSet.GetCurrent().Length; i++)
+            {
+                PrintStatus(diceSet, i);
+            }
+            Console.WriteLine($"It took {AutoRoll(diceSet,6)} attempts to get this result.");
+
+        }
+        static void PrintStatus(DiceSet diceSet)
+        {
+            int[] dices = diceSet.GetCurrent();
+
+            for (int i = 0; i < dices.Length; i++)
+            {
+                if (i == 0) Console.Write(dices[i]);
+                else Console.Write($" | {dices[i]}");
+            }
+
+            Console.WriteLine();
+        }
+        static void PrintStatus(DiceSet diceSet, int position)
+        {
+            Console.WriteLine($"Number on the dice #{position + 1} is {diceSet.GetCurrent(position)}.");
+        }
+
+
+        static int AutoRoll(DiceSet diceSet, int goal)
+        {
+            int attempts = 0;
+            int[] dices = diceSet.GetCurrent();
+            
+            for (int i = 0; i < dices.Length; i++)
+            {
+                while (dices[i] != goal)
+                {
+                    diceSet.Reroll(i);
+                    PrintStatus(diceSet);
+                    attempts++;
+                }
+            }
+            
+            return attempts;
+        }
+    }
+}
