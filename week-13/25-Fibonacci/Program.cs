76cf5bfb5c3cd1ca035ea10b7d24513c3756edfd
Karel <charles.mccoy@seznam.cz>
2020-02-29 23:29:44 +0100

    Fibonacci exercise added

---
+++ week-13/25-Fibonacci/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _25_Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Specify lenght of the Fibonacci Sequence (positive number): ");
+            uint length;
+            try
+            {
+                length = Convert.ToUInt32(Console.ReadLine().Trim());
+            }
+            catch (Exception)
+            {
+                length = 10;
+                Console.WriteLine("Invalid input - default length 10 has been selected!");
+            }
+
+            var fibSequence = new List<uint>();
+            Sequence(length, ref fibSequence);
+            Console.WriteLine(ToString(fibSequence));
+        }
+
+        public static uint Fibonacci(uint n)
+        {
+            //Fibonacci(n) = Fibonacci(n-1) + Fibonacci(n-2)
+            if (n == 0) return 0;
+            else if (n == 1) return 1;
+            else return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+
+        public static void Sequence(uint length, ref List<uint> output)
+        {
+            output.Insert(0,Fibonacci(length));
+            if (length != 0) Sequence(length - 1, ref output);
+        }
+
+        public static string ToString(List<uint> input)
+        {
+            string output = "";
+            for (int i = 0; i < input.Count; i++)
+            {
+                output += $"{input[i]}\n";
+            }
+            return output;
+        }
+    }
+}
---
+++ week-13/25-Fibonacci/Tests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace _25_Fibonacci
+{
+    public class Tests
+    {
+        //Fibonacci(n) = Fibonacci(n-1) + Fibonacci(n-2)
+        //Assert.Equal doesn't work for uint...
+        [Fact]
+        public void FibonacciTest0()
+        {
+            Assert.True(Program.Fibonacci(0) == 0);
+        }
+
+        [Fact]
+        public void FibonacciTest1()
+        {
+            Assert.True(Program.Fibonacci(1) == 1);
+        }
+
+        [Theory]
+        [InlineData(3, 2)]
+        [InlineData(8, 21)]
+        public void FibonacciTestSmall(uint n, uint expected)
+        {
+            Assert.True(Program.Fibonacci(n) == expected);
+        }
+
+        [Theory]
+        [InlineData(20)]
+        [InlineData(30)]
+        [InlineData(35)]
+        public void FibonacciTestLarge(uint n)
+        {
+            Assert.True(Program.Fibonacci(n) == (Program.Fibonacci(n-1) + Program.Fibonacci(n-2)));
+        }
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(8)]
+        [InlineData(23)]
+        public void SequenceTest(uint length) // length + "0"
+        {
+            var test = new List<uint>();
+            Program.Sequence(length, ref test);
+            Assert.True(length + 1 == test.Count);
+        }
+    }
+}
