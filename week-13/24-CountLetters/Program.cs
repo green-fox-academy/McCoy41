013badecbfe6d26c8276ec30c27addfa46d62c41
Karel <charles.mccoy@seznam.cz>
2020-02-29 17:56:02 +0100

    CountLetters exercise added

---
+++ week-13/24-CountLetters/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _24_CountLetters
+{
+    // To make stuff work, I had to add following to .csproj file in <PropertyGroup>
+    // <GenerateProgramFile>false</GenerateProgramFile>
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string input = Console.ReadLine().Replace(" ", "").ToUpper();
+
+            Console.WriteLine(ToString(CountLetters(input)));
+        }
+
+        public static string ToString(Dictionary<char, uint> input)
+        {
+            string output = "";
+
+            foreach (var item in input)
+            {
+                output += $" {item.Key} | {item.Value}\n";
+            }
+            return output;
+        }
+
+        public static Dictionary<char, uint> CountLetters(string input)
+        {
+            input = Sort(input.ToUpper().Replace(" ", ""));
+            var output = new Dictionary<char, uint>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!output.ContainsKey(input[i]))
+                {
+                    output.Add(input[i], 1);
+                }
+                else output[input[i]]++;
+            }
+            return output;
+        }
+
+        public static string Sort(string input)
+        {
+            char[] chars = input.ToCharArray();
+            Array.Sort(chars);
+            return new string(chars);
+        }
+    }
+}
---
+++ week-13/24-CountLetters/Tests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace _24_CountLetters
+{
+    public class Tests
+    {
+        [Fact]
+        public void TestEmptyOutput()
+        {
+            Assert.Equal("", Program.ToString(new Dictionary<char, uint>()));
+        }
+
+        [Fact]
+        public void TestOutput()
+        {
+            var input = new Dictionary<char, uint>();
+            input.Add('A', 5);
+            Assert.Equal(" A | 5\n", Program.ToString(input));
+        }
+
+        [Fact]
+        public void TestEmptyInput()
+        {
+            Assert.Equal(new Dictionary<char, uint>(), Program.CountLetters(""));
+        }
+
+        [Fact]
+        public void TestInput1()
+        {
+            var expected = new Dictionary<char, uint>();
+            expected.Add('B', 1);
+            expected.Add('E', 1);
+            expected.Add('L', 1);
+            expected.Add('K', 1);
+            expected.Add('R', 1);
+            Assert.Equal(expected, Program.CountLetters("Blerk"));
+        }
+        
+        [Fact]
+        public void TestInput2()
+        {
+            var expected = new Dictionary<char, uint>();
+            expected.Add('A', 1);
+            expected.Add('D', 1);
+            expected.Add('E', 1);
+            expected.Add('H', 2);
+            expected.Add('I', 2);
+            expected.Add('R', 2);
+            expected.Add('T', 2);
+            expected.Add('!', 3);
+            Assert.Equal(expected, Program.CountLetters("Hit it harder!!!"));
+        }
+
+        [Fact]
+        public void TestSort()
+        {
+            Assert.Equal("ABCD", Program.Sort("DACB"));
+        }
+    }
+}
